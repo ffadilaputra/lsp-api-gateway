1ad3970fc1a5d510ac4d3f1705435e18a45845c4
ffadilaputra <i.fadilaputra@gmail.com>
2019-05-22 13:04:24 +0700

    Congfig reverse proxy

--- src/main/java/org/ffadilaputra/gateway/apigateway/ApiGatewayApplication.java
+++ src/main/java/org/ffadilaputra/gateway/apigateway/ApiGatewayApplication.java
@@ -1,10 +1,15 @@
 package org.ffadilaputra.gateway.apigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
+@Configuration
+@Controller
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
