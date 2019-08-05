e26a4b2e8e033fb435a4217be0a723f990751f52
ffadilaputra <i.fadilaputra@gmail.com>
2019-08-05 14:54:18 +0700

    custim endpoint

--- src/main/java/org/ffadilaputra/gateway/apigateway/ApiGatewayApplication.java
+++ src/main/java/org/ffadilaputra/gateway/apigateway/ApiGatewayApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @Controller
